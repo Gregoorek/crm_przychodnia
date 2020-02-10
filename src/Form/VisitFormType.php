337099b945b2c2f035a57f4e385eaf5d59de1e15
Grzegorz Grudzień <grzes1987@gmail.com>
2020-02-10 12:58:37 +0100

    poprawa form wizyt z godzinami

--- src/Form/VisitFormType.php
+++ src/Form/VisitFormType.php
@@ -7,6 +7,7 @@ namespace App\Form;
 use App\Entity\Patient;
 use App\Entity\Visit;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,7 +19,9 @@ class VisitFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array  $options)
     {
         $builder
-            ->add('startDate')
+            ->add('startDate', DateTimeType::class,[
+                'minutes' => [0],
+                'hours' => [8,9,10,11,12,13,14,15]])
             ->add('doctor')
             ;
     }
